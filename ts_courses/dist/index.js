ab37131091ae4b5218d3833623cd318777aec436
Sergii Podriezienko <podreenko1986@gmail.com>
2023-12-26 14:09:55 +0200

    add: changes
    add: changes

---
+++ ts_courses/dist/index.js
@@ -0,0 +1,13 @@
+//let total: number = 100;
+//let name: string = 'bob';
+//let isActive: boolean = false;
+//let empty: null = null;
+//name = "5";
+//const numbers: number[] = [1, 2, 3, 4, 5, 6, 0];
+//numbers.push(12);
+const user = {
+    name: "Bob",
+    age: 12,
+};
+user.age = 15;
+user.name = '1';
